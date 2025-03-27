de89315bd34ea1daa04ac6af129bfce141937277
anhnhph33991 <hoang838604@gmail.com>
2025-03-27 16:23:06 +0700

    feat: install toastr and debugbar, config global

--- resources/views/admin/layouts/partials/side-bar-left.blade.php
+++ resources/views/admin/layouts/partials/side-bar-left.blade.php
@@ -13,48 +13,6 @@
                 </a>
             </li>
 
-            <li>
-                <a href="javascript: void(0);" class="waves-effect">
-                    <span class="badge rounded-pill bg-danger float-end" key="t-hot">Hot</span>
-                    <i class="bx bx-layout"></i>
-                    <span key="t-layouts">Layouts</span>
-                </a>
-                <ul class="sub-menu" aria-expanded="true">
-                    <li>
-                        <a href="javascript: void(0);" class="has-arrow" key="t-vertical">Vertical</a>
-                        <ul class="sub-menu" aria-expanded="true">
-                            <li><a href="layouts-light-sidebar.html" key="t-light-sidebar">Light Sidebar</a>
-                            </li>
-                            <li><a href="layouts-compact-sidebar.html" key="t-compact-sidebar">Compact
-                                    Sidebar</a></li>
-                            <li><a href="layouts-icon-sidebar.html" key="t-icon-sidebar">Icon Sidebar</a>
-                            </li>
-                            <li><a href="layouts-boxed.html" key="t-boxed-width">Boxed Width</a></li>
-                            <li><a href="layouts-preloader.html" key="t-preloader">Preloader</a></li>
-                            <li><a href="layouts-colored-sidebar.html" key="t-colored-sidebar">Colored
-                                    Sidebar</a></li>
-                            <li><a href="layouts-scrollable.html" key="t-scrollable">Scrollable</a></li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <a href="javascript: void(0);" class="has-arrow" key="t-horizontal">Horizontal</a>
-                        <ul class="sub-menu" aria-expanded="true">
-                            <li><a href="layouts-horizontal.html" key="t-horizontal">Horizontal</a></li>
-                            <li><a href="layouts-hori-topbar-light.html" key="t-topbar-light">Topbar
-                                    light</a></li>
-                            <li><a href="layouts-hori-boxed-width.html" key="t-boxed-width">Boxed width</a>
-                            </li>
-                            <li><a href="layouts-hori-preloader.html" key="t-preloader">Preloader</a></li>
-                            <li><a href="layouts-hori-colored-header.html" key="t-colored-topbar">Colored
-                                    Header</a></li>
-                            <li><a href="layouts-hori-scrollable.html" key="t-scrollable">Scrollable</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-
             <li class="menu-title" key="t-administration">Administration</li>
 
             <li>
@@ -64,12 +22,12 @@
                 </a>
                 <ul class="sub-menu" aria-expanded="false">
                     <li class="{{ activeMenuLi('admin/categories') }}">
-                        <a href="{{ route('admin.categories.index') }}" key="t-g-categories">
+                        <a href="#" key="t-g-categories">
                             Categories
                         </a>
                     </li>
                     <li class="{{ activeMenuLi('admin/sub-categories') }}">
-                        <a href="{{ route('admin.sub-categories.index') }}" key="t-v-maps" class="{{ activeMenu('admin/sub-categories') }}">
+                        <a href="#" key="t-v-maps" class="{{ activeMenu('admin/sub-categories') }}">
                             SubCategories
                         </a>
                     </li>
@@ -77,7 +35,7 @@
             </li>
 
             <li class="{{ activeMenuLi('admin/users') }}">
-                <a href="{{ route('admin.users.index') }}" class="waves-effect {{ activeMenu('admin/users') }}">
+                <a href="#" class="waves-effect {{ activeMenu('admin/users') }}">
                     <i class="bx bx-user"></i>
                     <span key="t-users">Users</span>
                 </a>
@@ -97,43 +55,44 @@
                 </a>
                 <ul class="sub-menu" aria-expanded="true">
                     <li>
-                        <a href="{{ route('admin.products.index') }}" key="t-level-1-1">
+                        <a href="#" key="t-level-1-1">
                             List Product
                         </a>
                     </li>
                     <li>
                         <a href="javascript: void(0);" class="has-arrow" key="t-level-1-2">Variants</a>
                         <ul class="sub-menu" aria-expanded="true">
-                            <li><a href="{{ route('admin.product-colors.index') }}" key="t-level-2-1">Colors</a></li>
-                            <li><a href="{{ route('admin.product-sizes.index') }}" key="t-level-2-2">Sizes</a></li>
+                            <li><a href="#" key="t-level-2-1">Colors</a></li>
+                            <li><a href="#" key="t-level-2-2">Sizes</a></li>
                         </ul>
                     </li>
                 </ul>
             </li>
 
             <li class="{{ activeMenuLi('admin/comments') }}">
-                <a href="{{ route('admin.comments.index') }}" class="waves-effect">
+                <a href="#" class="waves-effect">
                     <i class="bx bx-receipt"></i>
                     <span key="t-comments">Comments</span>
                 </a>
             </li>
 
             <li>
-                <a href="{{ route('admin.orders.index') }}" class="waves-effect">
+                <a href="#" class="waves-effect">
+                    <span class="badge rounded-pill bg-danger float-end" key="t-hot">2</span>
                     <i class="bx bx-receipt"></i>
                     <span key="t-orders">Orders</span>
                 </a>
             </li>
 
             <li>
-                <a href="{{ route('admin.flash-sales.index') }}" class="waves-effect">
+                <a href="#" class="waves-effect">
                     <i class="bx bx-receipt"></i>
                     <span key="t-flash-sale">Flash Sale</span>
                 </a>
             </li>
 
             <li>
-                <a href="{{ route('admin.coupons.index') }}" class="waves-effect">
+                <a href="#" class="waves-effect">
                     <i class="bx bx-receipt"></i>
                     <span key="t-coupons">Coupons</span>
                 </a>
@@ -142,7 +101,7 @@
             <li class="menu-title" key="t-settings">Settings</li>
 
             <li>
-                <a href="{{ route('admin.banner.index') }}" class="waves-effect">
+                <a href="#" class="waves-effect">
                     <i class="bx bx-receipt"></i>
                     <span key="t-banner">Banner</span>
                 </a>
@@ -150,7 +109,7 @@
 
 
             <li>
-                <a href="{{ route('admin.menus.index') }}" class="waves-effect">
+                <a href="#" class="waves-effect">
                     <i class="bx bx-receipt"></i>
                     <span key="t-menu">Menu</span>
                 </a>
