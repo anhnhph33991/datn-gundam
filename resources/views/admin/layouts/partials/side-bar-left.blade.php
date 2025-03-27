5b8a71b8ec219b04f83815ae243e7125ececba52
anhnhph33991 <hoang838604@gmail.com>
2025-03-27 23:12:39 +0700

    feat: crud size for admin

--- resources/views/admin/layouts/partials/side-bar-left.blade.php
+++ resources/views/admin/layouts/partials/side-bar-left.blade.php
@@ -44,7 +44,7 @@
                         <a href="javascript: void(0);" class="has-arrow" key="t-level-1-2">Variants</a>
                         <ul class="sub-menu" aria-expanded="true">
                             <li><a href="#" key="t-level-2-1">Colors</a></li>
-                            <li><a href="#" key="t-level-2-2">Sizes</a></li>
+                            <li><a href="{{ route('admin.product-sizes.index') }}" key="t-level-2-2">Sizes</a></li>
                         </ul>
                     </li>
                 </ul>
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashBoardController;
+use App\Http\Controllers\Admin\ProductSizeController;
 use App\Http\Controllers\Admin\SubCategoryController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashBoardController::class, 'index'])->name('dashboard');
 Route::resource('categories', CategoryController::class);
 Route::resource('users', UserController::class);
+
+Route::resource('product-sizes', ProductSizeController::class);
